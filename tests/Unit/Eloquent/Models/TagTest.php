36976378b6245d5a89ba7440440aaec2372a5309
Matthew Daly <matthewbdaly@gmail.com>
2018-04-15 20:37:08 +0100

    Added outline of first test

---
+++ tests/Unit/Eloquent/Models/TagTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Eloquent\Models;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Matthewbdaly\LaravelTags\Eloquent\Models\Tag;
+
+class TagTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
